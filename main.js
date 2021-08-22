d1fca3e36e6bf255dc5103040125e4efbbe22c1d
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2021-08-22 11:51:10 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+var youWin = false;
+var numbers = [10,10,10,10,10,10,10,10];
+var downPower = 1;
+var autoDown = 0;
+function numberGoesDown() {
+  number -= downPower;
+  updateNumbers();
+}
+function numberGoesDowner() {
+  downPower++;
+  if (downPower > 10) downPower = 10;
+}
+function numberGoesDownAuto() {
+  autoDown++;
+  if (autoDown > 10) autoDown = 10;
+}
+function updateNumbers() {
+  for (let i in numbers) {
+    if (numbers[i] < 0) {
+      if (i == 7) {
+        youWin = true;
+      } else {
+        numbers[i] = 10-numbers[i]%10;
+        numbers[i+1] += Math.floor(numbers[i]/10);
+      }
+    }
+    document.getElementById("number"+i) = numbers[i].toFixed(2);
+  }
+}
+var lastFrame = 0;
+function nextFrame(timeStamp) {
+  let dt = timeStamp - lastFrame;
+  lastFrame = timeStamp;
+  numbers[0] -= autoDown*dt/1000;
+  if (downPower > 1) {
+    downPower -= dt/1000;
+  } else downPower = 1;
+  if (autoDown > 0) {
+    autoDown -= dt/100;
+  } else autoDown = 0;
+  updateNumbers();
+  if (youWin) document.write("You're winner!")
+  window.requestAnimationFrame(nextFrame);
+}
+window.requestAnimationFrame(nextFrame);
